4b88c244c07cb8ee9726eb0dd1274feaf387b50c
adahmed-coursera <96035511+adahmed-coursera@users.noreply.github.com>
2022-04-19 18:51:12 +0700

    update apiUrl
    update apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://89614bd2.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
